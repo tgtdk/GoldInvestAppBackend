58d073db08ecaa843064901f99401e4c65160a52
tgtdk <dhruvilharane188@gmail.com>
2025-02-08 16:14:01 +0530

    Model and interface added

---
+++ GoldInvestApp/Models/GoldInvestModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoldInvestApp.Models
+{
+    public class GoldInvestModelRequest
+    {
+        [Required(ErrorMessage = "Year Is Required")]
+        public string cYear { get; set; }
+    }
+    public class GoldInvestModelResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public string cPrice { get; set; }
+    }
+}
---
+++ GoldInvestApp/Repository/Interface/IGoldInvest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoldInvestApp.Repository.Interface
+{
+    interface IGoldInvest
+    {
+    }
+}
